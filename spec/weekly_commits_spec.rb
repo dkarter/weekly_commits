22f75ea50598c31fa89bca8cbf8bce325c3fbc67
Joshua Plicque <plicjo@gmail.com>
2020-08-31 20:04:19 -0400

    Add no merge option

--- spec/weekly_commits_spec.rb
+++ spec/weekly_commits_spec.rb
@@ -4,4 +4,16 @@ describe WeeklyCommits do
   it 'has a version number' do
     expect(WeeklyCommits::VERSION).not_to be nil
   end
+
+  context 'default options' do
+    let(:default_args) { [] }
+    let(:default_options) do
+      { week: 0, sort: 'asc' }
+    end
+
+    it 'lists commits for Monday through Friday' do
+      days_printed = 5
+      expect(WeeklyCommits::CLI.new(default_args, default_options).weekly_commits).to eq(days_printed)
+    end
+  end
 end
